b4be592c20dc8ccfd26af3b689c324d375211748
r-kido <r-kido@CD26306L.arksystems.co.jp>
2026-04-16 11:29:13 +0900

    question3完了

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
@@ -9,6 +9,18 @@ public class Question03_2 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
-		
+		int basePrice = 1200;
+		int tickets = 3;
+		int reducedPrice = basePrice - 200;
+		int reducedTikets = --tickets;
+		int totalPrice = reducedPrice * reducedTikets;
+		double taxRate = 1.1;
+		int totalWithTax = (int) (totalPrice * taxRate);
+
+		System.out.println("購入可能枚数は" + reducedTikets + "枚です。");
+		System.out.println("値下げ後の1枚当たりの料金は" + reducedPrice + "円です。");
+		System.out.println("合計金額(税抜)は" + totalPrice + "です。");
+		System.out.println("合計金額(税込)は" + totalWithTax + "です。");
+
 	}
 }
